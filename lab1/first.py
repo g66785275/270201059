422b0191f0fe2355a55f5b0772a5c41763e0c9c1
g66785275 <cer79cag@gmail.com>
2020-10-19 09:49:50 +0000

    done

--- lab1/first.py
+++ lab1/first.py
@@ -3,7 +3,15 @@
 #print(fahrenheit)
 
 
-edge1 = float(input("Edge1:"))
-edge2 = float(input("Edge:"))
-hypotenuse = (edge1**2+edge2**2)**0.5
-print("Hypotenuse:",hypotenuse)
+#edge1 = float(input("Edge1:"))
+#edge2 = float(input("Edge:"))
+#hypotenuse = (edge1**2+edge2**2)**0.5
+#print("Hypotenuse:",hypotenuse)
+
+
+car1 = 80
+car2 = 70
+distance = 490
+distance2 = 150
+time = ((distance-distance2)/(car1+car2))*60
+print(time)
