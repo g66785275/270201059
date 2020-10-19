35fe079041850155b139034d39fd8bac8ae99ebc
g66785275 <cer79cag@gmail.com>
2020-10-19 09:37:32 +0000

    done

--- lab1/first.py
+++ lab1/first.py
@@ -1,3 +1,9 @@
-temperature = float(input("Enter a temperature:"))
-fahrenheit = temperature*1.8+32
-print(fahrenheit)
+#temperature = float(input("Enter a temperature:"))
+#fahrenheit = temperature*1.8+32
+#print(fahrenheit)
+
+
+edge1 = float(input("Edge1:"))
+edge2 = float(input("Edge:"))
+hypotenuse = (edge1**2+edge2**2)**0.5
+print("Hypotenuse:",hypotenuse)
