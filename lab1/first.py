64ef22d554d7ead853c57610e152f6820c68eb90
g66785275 <cer79cag@gmail.com>
2020-10-25 11:21:47 +0000

    my homework

--- lab1/first.py
+++ lab1/first.py
@@ -9,9 +9,18 @@
 #print("Hypotenuse:",hypotenuse)
 
 
-car1 = 80
-car2 = 70
-distance = 490
-distance2 = 150
-time = ((distance-distance2)/(car1+car2))*60
-print(time)
+#car1 = 80
+#car2 = 70
+#distance = 490
+#distance2 = 150
+#time = ((distance-distance2)/(car1+car2))*60
+#print(time)
+
+
+easy_pace = 8*1
+tempo = 6*3
+easy_pace_again = 8*2
+total_minutes = easy_pace+easy_pace_again+tempo
+minutes = (52 + total_minutes) - 60
+
+print("I get home for breakfast at ","7:",minutes,"o'clock.")
