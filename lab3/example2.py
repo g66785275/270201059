0516b9b7a060be82d83fb65e90421b9fbdcd1fe2
g66785275 <cer79cag@gmail.com>
2020-10-26 08:56:59 +0000

    i did

---
+++ lab3/example2.py
@@ -0,0 +1,15 @@
+number1 = float(input("Number1:"))
+number2 = float(input("Number5:"))
+number3= float(input("Number3:"))
+
+#liste=list()
+
+#liste.append(number1)
+#liste.append(number2)
+#liste.append(number3)
+
+#list.sort()
+#print(liste[0])
+
+min_num = min(number1,number2,number3)
+print(min_num)
