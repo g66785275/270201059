4e7dc00d4cda9725f0f61e97644771d4fb996028
g66785275 <cer79cag@gmail.com>
2020-11-09 09:26:38 +0000

    i did

--- lab4/example4.py
+++ lab4/example4.py
@@ -1,6 +1,11 @@
-import math
-a = int(input("A:"))
-b = int(input("B:"))
-print(math.pow(a,b))
+#import math
+#a = int(input("A:"))
+#b = int(input("B:"))
+#print(math.pow(a,b))
 
-
+a=int(input())
+b=int(input())
+total=a
+for i in range(0,b-1):
+  total*=a
+print(total)
