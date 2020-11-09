0dd247c151da0e4313ad0f1b11b3c60ec0235710
g66785275 <cer79cag@gmail.com>
2020-11-09 08:54:26 +0000

    a

--- lab4/example1.py
+++ lab4/example1.py
@@ -1,6 +1,13 @@
 
-giriş = input("Number:")
-if len(giriş)>1:
-  print("Two last digits:",giriş[-2:])
-elif len(giriş) == 1:
-  print("Two last digits:","0",giriş)
+#giriş = input("Number:")
+#if len(giriş)>1:
+ # print("Sum of last digits:",int(giriş[-1]) + int(giriş[-2]))
+
+
+
+number = int(input("Enter an integer:"))
+
+ones = number % 10
+tens = (number % 100) // 10
+total = ones + tens
+print(total)
