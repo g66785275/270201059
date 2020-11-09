afcea1c376e3e1489fcbd5aa7e7802433a332b81
g66785275 <cer79cag@gmail.com>
2020-11-09 09:18:57 +0000

    a

--- lab4/example3.py
+++ lab4/example3.py
@@ -1,5 +1,9 @@
+import math
 nums = [8,60,43,55,25,134,1]
 number = 0
 for q in nums:
   number += q
 print(number)
+
+
+
---
+++ lab4/example4.py
@@ -0,0 +1,6 @@
+import math
+a = int(input("A:"))
+b = int(input("B:"))
+print(math.pow(a,b))
+
+
