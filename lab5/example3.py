eddf57938135e953b5755276fa0f671738f1f46d
g66785275 <cer79cag@gmail.com>
2020-11-16 09:39:25 +0000

    done

--- lab5/example3.py
+++ lab5/example3.py
@@ -2,7 +2,7 @@ one = input("Enter a number:")
 two = input("Enter a number:")
 index1 = 0
 length = min(len(one),len(two))
-for _ in range(length) :
-  if one[_] == two[_]:
+for i in range(length) :
+  if one[i] == two[i]:
     index1 += 1
 print(index1)
---
+++ lab5/example4.py
@@ -0,0 +1,11 @@
+password = "abc123"
+entry = input("Password:")
+index = 0
+while True:
+  if entry != password:
+    print("Wrong")
+  elif entry = "help":
+    print(password[index])
+    index += 1 
+  elif entry == password:
+    print("Welcome ! ")
