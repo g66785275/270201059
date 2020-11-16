339e6cd30e9ee06fc32e43724d27f97dc88d9911
g66785275 <cer79cag@gmail.com>
2020-11-16 09:29:01 +0000

    done

--- lab5/example3.py
+++ lab5/example3.py
@@ -1,8 +1,8 @@
 one = input("Enter a number:")
 two = input("Enter a number:")
 index1 = 0
-
-for a in one :
-  if a in two:
+length = min(len(one),len(two))
+for _ in range(length) :
+  if one[_] == two[_]:
     index1 += 1
 print(index1)
