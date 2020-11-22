2c9cfe4878985482ff3e584fad494e11a20ce0a9
g66785275 <cer79cag@gmail.com>
2020-11-22 13:31:58 +0000

    fibonacci numbers!

--- lab5/example5.py
+++ lab5/example5.py
@@ -1,5 +1,17 @@
 a = int(input("How many fibonacci numbers?"))
 first = 1
 second = 1
-
+third = 2
+for q in range(a):
+  if a == 1 or a == 2:
+    print(1)
+    break
+  elif a ==3:
+    print(2)
+    break
+  else:
+    third1 = third
+    third = third + second
+    second = third1
+print(third)
 
