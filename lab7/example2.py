16fa701fcfce02555f260d17d4b72daddcb90a2a
g66785275 <cer79cag@gmail.com>
2020-11-30 08:57:35 +0000

    done

---
+++ lab7/example2.py
@@ -0,0 +1,9 @@
+books = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
+a = dict()
+for q in books:
+  length = len(q)
+  for w in q:
+    if q.count(w)>1:
+      length1 = length - q.count(w)+1
+  a[q] = (length,length1)
+print(a)
