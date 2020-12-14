af41b451608fa4490516d43e07ae88c0dbbf5e9b
g66785275 <cer79cag@gmail.com>
2020-12-14 08:53:43 +0000

    done

--- lab7/example5.py
+++ lab7/example5.py
@@ -1,9 +1,16 @@
 
+k = 0
 while True:
-  keyp=input(False)
+
+  keyp=input()
   if len(keyp) < 8:
     print(False)
   else:
+    for q in keyp:
+      if q != int(q):
+        k+=1
+    if k == len(keyp):
+        print(False)
     if keyp.lower() == keyp:
       print(False)
     else:
