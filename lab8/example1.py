a8a08c677b3e47139a5157b6328a52ddc6b0a185
g66785275 <cer79cag@gmail.com>
2020-12-14 08:57:17 +0000

    done

--- lab8/example1.py
+++ lab8/example1.py
@@ -1,7 +1,8 @@
 #a_list = [12, -7, 5, -89.4, 3, 27, 56, 57.3]
+
 def a(a_list):
-  a = 0
+  b = 0
   for q in a_list:
-    a += q
-  print(q**2)   
+    b = b + q
+  print(b)   
 a([12, -7, 5, -89.4, 3, 27, 56, 57.3])   
